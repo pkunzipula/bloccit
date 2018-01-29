fd325c4271ca75f00b05a478c56a98d92c2dc110
pkunzipula <jcapra76@gmail.com>
2018-01-29 03:39:14 +0000

    Signing up is good

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
   
-  resources :users, only: [:new, :create, :confirm]
+  resources :users, only: [:new, :create]
+  
+  resources :sessions, only: [:new, :create, :destroy]
   
   resources :questions
 
